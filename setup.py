a36c224ce3f1def4638ebe850032a5aed418d29f
Andy Casey <acasey@mso.anu.edu.au>
2010-11-17 22:21:17 +1100

    setup.py actually added

---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+try: # Python 3.x
+    from distutils.command.build_py import build_py_2to3 as build_py
+except ImportError: # Python 2.x
+    from distutils.command.build_py import build_py
+
+
+setup(name='atmosphy',
+      version='0.1',
+      description='Stellar atmosphere models in Python',
+      author='Wolfgang Kerzendorf and Andy Casey',
+      author_email='wkerzendorf@mso.anu.edu.au, \
+        acasey@mso.anu.edu.au',
+      url='',
+      packages = ['atmosphy'],
+      cmdclass = {'build_py':build_py},
+     )
