2907cd5e0c0c9b094ab5c539c24848f2c0ab9d22
Andy Casey <acasey@mso.anu.edu.au>
2011-02-11 15:34:14 +1100

    updated modeldb.py, fileio.py is a work in progress to fix the removal of initialize

--- modeldb.py
+++ modeldb.py
@@ -7,45 +7,47 @@ def convertBzipPickle(bzipPickle):
     return pickle.loads(bz2.decompress(bzipPickle))
 
 class modelDBException(Exception):
-	pass
+    pass
 
 def getModelDBConnection():
-	"returns a connection to the modelDB"
+    "returns a connection to the modelDB"
         
-	return sqlite3.connect(initialize.atmosStoragePath('atmosphy.db3'),
-                               detect_types=sqlite3.PARSE_DECLTYPES)
-	
+    return sqlite3.connect(initialize.atmosStoragePath('atmosphy.db3'),
+                detect_types=sqlite3.PARSE_DECLTYPES)
+
+
+
 def initModelTable(modelName, clobber=False):
-	"Creating Model Table"
+    "Creating Model Table"
 
-	conn = getModelDBConnection()
-	if len(conn.execute(
-		"SELECT name FROM sqlite_master WHERE type='table' AND	name='%s'" 
-		% (modelName,)).fetchall()) == 1:
-		if clobber:
-			conn.execute('DROP TABLE `%s`' % (modelName,))
-		else:
-			raise modelDBException("Model %s already exists in database"
-								 % (modelName,))
+    conn = getModelDBConnection()
+    if len(conn.execute(
+        "SELECT name FROM sqlite_master WHERE type='table' AND    name='%s'" 
+        % (modelName,)).fetchall()) == 1:
+        if clobber:
+            conn.execute('DROP TABLE `%s`' % (modelName,))
+        else:
+            raise modelDBException("Model %s already exists in database"
+                                 % (modelName,))
 
-	initModelTable = """CREATE TABLE `%s` (	id INTEGER PRIMARY KEY,
-    										teff DOUBLE,
-    										logg DOUBLE,
-    										feh DOUBLE,
-    										k DOUBLE,
-    										alpha DOUBLE,
-    										lh DOUBLE,
-    										pradk DOUBLE,
-    										deck BZPKL)""" % modelName
-	conn.execute(initModelTable)
-	conn.commit()
-	conn.close()
-	
+    initModelTable = """CREATE TABLE `%s` (    id INTEGER PRIMARY KEY,
+                                            teff DOUBLE,
+                                            logg DOUBLE,
+                                            feh DOUBLE,
+                                            k DOUBLE,
+                                            alpha DOUBLE,
+                                            lh DOUBLE,
+                                            pradk DOUBLE,
+                                            deck BZPKL)""" % modelName
+    conn.execute(initModelTable)
+    conn.commit()
+    conn.close()
+    
 def insertModelData(conn, modelName, dataTuple):
-	"Insert data into the model database"
-	dataTuple[-1] = sqlite3.Binary(dataTuple[-1])
-	conn.execute(
-		'insert into `%s` (teff, logg, feh, k, alpha, lh, pradk, deck)'
-		'values (?,?,?,?,?,?,?,?)' % (modelName,), tuple(dataTuple))
+    "Insert data into the model database"
+    dataTuple[-1] = sqlite3.Binary(dataTuple[-1])
+    conn.execute(
+        'insert into `%s` (teff, logg, feh, k, alpha, lh, pradk, deck)'
+        'values (?,?,?,?,?,?,?,?)' % (modelName,), tuple(dataTuple))
 
 sqlite3.register_converter("BZPKL", convertBzipPickle)
